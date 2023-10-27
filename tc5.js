94ca7f9d6beaa9f3b3baddd532fda79fbc85ad64
hamletnscc <113032060+hamletnscc@users.noreply.github.com>
2023-10-27 08:30:18 -0300

    Update tc5.js with Number.isInteger hint

--- tc5.js
+++ tc5.js
@@ -12,7 +12,16 @@ function getHighestCommonDivisor(in_num1, in_num2)
   
 }
 
+/*
+Tips: The Number.isInteger() method returns true if a value is an integer of the datatype Number. 
+Otherwise it returns false.
 
+Examples:
+Number.isInteger(-123); //return true
+Number.isInteger('123'); //return false
+Number.isInteger(12.3); //return false
+Number.isInteger("Great!"); //return false
+*/
 function validateNumericInput(in_message)
 {
  
