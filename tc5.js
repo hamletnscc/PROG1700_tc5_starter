23d3b975545629441ae99e8c7e69458e92fdcb41
hamletnscc <113032060+hamletnscc@users.noreply.github.com>
2023-10-27 08:21:29 -0300

    Create tc5.js

---
+++ tc5.js
@@ -0,0 +1,39 @@
+"use strict";
+var readlineSync = require('readline-sync');
+
+/////////////////////////////////////
+// PROG1700 - Tech Check 5 SOLUTION
+// Highest Common Divisor
+// Loops
+/////////////////////////////////////
+
+function getHighestCommonDivisor(in_num1, in_num2)
+{
+  
+}
+
+
+function validateNumericInput(in_message)
+{
+ 
+}
+    
+
+function main()
+{
+ 
+}
+
+
+//PROGRAM STARTS HERE
+if (require.main === module)
+{
+    main();
+}
+
+
+//do not change the following lines
+module.exports = {
+   getHighestCommonDivisor,
+   validateNumericInput
+}
